afce927a4a6a8892848a0adc02250983ddd4502f
mahopon <tan.cy@outlook.com>
2025-08-18 15:46:08 +0800

    new:Add util for telegram parsing

---
+++ internal/util/escape_chars.go
@@ -0,0 +1,27 @@
+package util
+
+import "strings"
+
+func EscapeMarkdownV2(text string) string {
+	replacer := strings.NewReplacer(
+		"_", "\\_",
+		"*", "\\*",
+		"[", "\\[",
+		"]", "\\]",
+		"(", "\\(",
+		")", "\\)",
+		"~", "\\~",
+		"`", "\\`",
+		">", "\\>",
+		"#", "\\#",
+		"+", "\\+",
+		"-", "\\-",
+		"=", "\\=",
+		"|", "\\|",
+		"{", "\\{",
+		"}", "\\}",
+		".", "\\.",
+		"!", "\\!",
+	)
+	return replacer.Replace(text)
+}
